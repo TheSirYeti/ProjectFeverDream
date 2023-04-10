c46c397c6a7baf628d57b8c4ef0ed2d7c05e9b50
JP Rshaid <jprshaid123@gmail.com>
2023-04-10 01:46:50 -0300

    Dummy?

---
+++ Assets/0_Scripts/Enemigos/HumanoidDummy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanoidDummy : Enemy
+{
+    
+    
+    public override void Attack()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void Move()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void Death()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void DoKnockback()
+    {
+        //throw new System.NotImplementedException();
+    }
+}
