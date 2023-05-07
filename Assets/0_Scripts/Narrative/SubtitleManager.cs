07aa052e4d45a22787a6e922d9439e3614f52a06
JP Rshaid <jprshaid123@gmail.com>
2023-05-07 03:11:14 -0300

    Mas level, Subtitle fix, Tutorial Robot Updated

--- Assets/0_Scripts/Narrative/SubtitleManager.cs
+++ Assets/0_Scripts/Narrative/SubtitleManager.cs
@@ -32,6 +32,7 @@ public class SubtitleManager : MonoBehaviour
         SoundManager.instance.SetNewVoiceSet(allSfx);
         
         //temp?
+        StopCoroutine(PlayVoiceline());
         StartCoroutine(PlayVoiceline());
     }
 
