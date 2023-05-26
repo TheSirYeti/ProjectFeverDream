e8967bdccfad64d84ddfe07a8bf985adb074c07e
MaiineV <agustinmartinezbarrios2@hotmail.com>
2023-05-26 19:29:04 -0300

    SO for music and sound in each scene

--- Assets/0_Scripts/Utilities/SO_AudioSet.cs
+++ Assets/0_Scripts/Utilities/SO_AudioSet.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "Generic Audio Clip Set", menuName = "ScriptableObjects/AudioClip")]
 public class SO_AudioSet : ScriptableObject
 {
     public AudioClip[] actualLevelAudioClip;
+    public List<string> audioClipNames;
 }
