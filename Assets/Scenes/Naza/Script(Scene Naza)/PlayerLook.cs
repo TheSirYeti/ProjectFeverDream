020d25f8e32ac81775287cba8638a737aff2a64a
Aterwind <82958160+Aterwind@users.noreply.github.com>
2023-05-27 19:06:38 -0300

    Playermove para moverme en la escena de la luces

---
+++ Assets/Scenes/Naza/Script(Scene Naza)/PlayerLook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] WallRun wallRun;
+
+    [SerializeField] private float sens ;
+
+    [SerializeField] Transform cam = null;
+    [SerializeField] Transform orientation = null;
+
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 100f;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sens;
+        xRotation -= mouseY * sens;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.transform.rotation = Quaternion.Euler(xRotation, yRotation, wallRun.tilt);
+        orientation.transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
