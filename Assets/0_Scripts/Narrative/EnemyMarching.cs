db2b00d18d385cf049280b990d7fcb50455d570d
JP Rshaid <jprshaid123@gmail.com>
2023-06-04 02:12:38 -0300

    Mas level

---
+++ Assets/0_Scripts/Narrative/EnemyMarching.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMarching : GenericObject
+{
+    [SerializeField] private float timeToMarch;
+    [SerializeField] private GameObject enemies;
+    [SerializeField] private Transform startPosition, endPosition;
+    
+    private void Awake()
+    {
+        UpdateManager._instance.AddObject(this);
+    }
+
+    public override void OnStart()
+    {
+        StartCoroutine(DoMarch());
+    }
+
+    IEnumerator DoMarch()
+    {
+        while (true)
+        {
+            Vector3 startPos = new Vector3(startPosition.position.x, enemies.transform.position.y, enemies.transform.position.z);
+            LeanTween.moveX(enemies, endPosition.position.x, timeToMarch);
+            yield return new WaitForSeconds(timeToMarch);
+            enemies.transform.position = startPos;
+        }
+    }
+}
