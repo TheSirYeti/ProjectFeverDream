6ccb66e41c05e9c315e8557ebe8c036c0aded2da
MaiineV <agustinmartinezbarrios2@hotmail.com>
2023-06-09 01:55:23 -0300

    Nodes work in progress

--- Assets/0_Scripts/NewIATesting/MNode.cs
+++ Assets/0_Scripts/NewIATesting/MNode.cs
@@ -99,7 +99,7 @@ public class MNode : MonoBehaviour
         }
     }
 
-    private void OnDrawGizmosSelected()
+    private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, 0.2f);
