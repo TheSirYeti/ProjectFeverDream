ebbcf7646b8e8e8fa472e13248db5ada5a252fc2
JP Rshaid <jprshaid123@gmail.com>
2023-06-25 21:42:51 -0300

    Cutscene Robot encounter, fixes, update gitignore

---
+++ Assets/0_Scripts/Physics/RagdollToggler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollToggler : GenericObject
+{
+    private Rigidbody rb;
+    private Animator animator;
+    public List<Rigidbody> ragdollRigidbodies;
+    private void Awake()
+    {
+        UpdateManager._instance.AddObject(this);
+    }
+
+    public override void OnStart()
+    {
+        animator = GetComponent<Animator>();
+        //rb = GetComponent<Rigidbody>();
+        StopRagdoll();
+    }
+    
+    public void DoRagdoll()
+    {
+        foreach (var rigidbody in ragdollRigidbodies)
+        {
+            if(rigidbody != rb)
+                rigidbody.isKinematic = false;
+        }
+        
+        //rb.isKinematic = true;
+        animator.enabled = false;
+    }
+
+    public void StopRagdoll()
+    {
+        foreach (var rigidbody in ragdollRigidbodies)
+        {
+            if(rigidbody != rb)
+                rigidbody.isKinematic = true;
+        }
+        
+        //rb.isKinematic = false;
+        animator.enabled = true;
+    }
+}
