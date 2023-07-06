a2e8106dbf2c8fd1ad4ac7d315ab32a529225c08
MaiineV <agustinmartinezbarrios2@hotmail.com>
2023-07-06 01:10:44 -0300

    New enemy setup

---
+++ Assets/0_Scripts/NewIA/MEnemy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MEnemy : GenericObject
+{
+    protected float _life;
+    protected bool _isDeath;
+    
+    protected float _speed;
+    
+    
+}
