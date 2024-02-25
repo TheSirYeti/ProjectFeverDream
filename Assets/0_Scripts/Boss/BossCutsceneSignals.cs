f7a9ba573c32b12f20da25a3e390c89cb2b1828b
JP Rshaid <jprshaid123@gmail.com>
2024-02-25 02:07:06 -0300

    Nueva EndCutscene, Fixes

--- Assets/0_Scripts/Boss/BossCutsceneSignals.cs
+++ Assets/0_Scripts/Boss/BossCutsceneSignals.cs
@@ -19,4 +19,11 @@ public class BossCutsceneSignals : GenericObject
     {
         EventManager.Trigger("OnNextRecipe");
     }
+    
+    public void OnLevelOver()
+    {
+        EventManager.Trigger("OnCutsceneEvent", true);
+        EventManager.Trigger("OnPPCalled", PPNames.BORDERCINEMATIC, false);
+        GameManager.Instance.NextScene();
+    }
 }
